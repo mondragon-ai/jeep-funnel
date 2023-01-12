cc87d403e26706594e918dfa2fea9270bdada517
SammyChris <ebusameric@gmail.com>
2023-01-12 15:37:19 +0100

    feature: add sessionStorage to context for persistence purposes

---
+++ context/index.js
@@ -0,0 +1,50 @@
+import { createContext, useEffect, useState } from "react";
+import { getItem, saveItem } from "../lib/storage";
+
+export const Context = createContext({});
+
+export const ContextProvider = ({ children }) => {
+  const [state, setState] = useState({
+    first_name: "",
+    email: "",
+    cus_uuid: "",
+    clientSecret: "",
+    funnel_uuid: "fun_7626c00357",
+    products: [],
+    high_risk: false,
+    bump: false,
+  });
+  useEffect(() => {
+    const {
+      first_name,
+      email,
+      cus_uuid,
+      clientSecret,
+      funnel_uuid,
+      products,
+      high_risk,
+      bump,
+    } = getItem("funnel_data") || state;
+    setState({
+      first_name,
+      email,
+      cus_uuid,
+      clientSecret,
+      funnel_uuid,
+      products,
+      high_risk,
+      bump,
+    });
+  }, []);
+
+  const setGlobalState = (data) => {
+    setState({ ...state, ...data });
+    saveItem("funnel_data", { ...state, ...data });
+  };
+  const globalState = state;
+  return (
+    <Context.Provider value={[globalState, setGlobalState]}>
+      {children}
+    </Context.Provider>
+  );
+};
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,24 +1,13 @@
 import "../styles/globals.css";
-import { useState } from "react";
 import Layout from "../components/Layout";
-import GlobalContext from "../context/globalContext";
+import { ContextProvider } from "../context";
 
 export default function App({ Component, pageProps }) {
-  const [globalState, setGloblaState] = useState({
-    first_name: "",
-    email: "",
-    cus_uuid: "",
-    clientSecret: "",
-    funnel_uuid: "fun_7626c00357",
-    products: [],
-    high_risk: false,
-    bump: false,
-  });
   return (
-    <GlobalContext.Provider value={[globalState, setGloblaState]}>
+    <ContextProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </GlobalContext.Provider>
+    </ContextProvider>
   );
 }
