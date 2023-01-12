a2d702d52fc044510b711c3d2f1dbd3051f3cdda
SammyChris <ebusameric@gmail.com>
2023-01-12 15:43:16 +0100

    Update import statements and update global state for OrderFormContainer

--- containers/OrderFormContainer.jsx
+++ containers/OrderFormContainer.jsx
@@ -1,19 +1,25 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { imPoweredRequest } from "../lib/request";
 import { useElements, useStripe } from "@stripe/react-stripe-js";
 import OrderForm from "../components/OrderForm";
-import GlobalContext from "../context/globalContext";
+import { Context } from "../context";
 
 function OrderFormContainer() {
   const stripe = useStripe();
   const elements = useElements();
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState("");
-  const [globalState, setGlobalState] = useContext(GlobalContext);
+  const [clientOrigin, setClientOrigin] = useState("http://localhost:3000");
+  const [globalState, setGlobalState] = useContext(Context);
 
   const initialValues = {
-    product:
-      '{ "name": "30 GIVEAWAY ENTRIES (BEST DEAL!!)", "price": "5000",  "piece": "$5.00 per pc", "product_id": "42235974189228" }',
+    product: {
+      title: "30 GIVEAWAY ENTRIES (BEST DEAL!!)",
+      price_str: "$5.00 / pc",
+      price_num: 5000,
+      piece: "6 Products (3 Wristbands/3 Decals) Save 30%",
+      product_id: "42235974189228",
+    },
     shipping: {
       line1: "",
       state: "",
@@ -23,70 +29,111 @@ function OrderFormContainer() {
     bump: false,
   };
 
+  useEffect(() => {
+    setClientOrigin(window.location.origin);
+  }, []);
+
   const handleSubmit = async (values, { setSubmitting }) => {
-    await fetchCustomerData(values);
+    try {
+      setIsLoading(true);
 
-    if (!stripe || !elements) return;
+      if (!stripe || !elements) {
+        throw new Error("stripe or elements are not present");
+      }
 
-    const { error } = await stripe.confirmSetup({
-      elements,
-      confirmParams: {
-        return_url: "http://localhost:3000/upsell",
-      },
-    });
+      updateGlobalState(values); // update global state with the order data
 
-    if (error.type === "card_error" || error.type === "validation_error")
+      setTimeout(() => {
+        fetchCustomerData(values); // simulate a delay
+      }, 0);
+
+      const { error } = await stripe.confirmSetup({
+        elements,
+        confirmParams: {
+          return_url: `${clientOrigin}/upsell`,
+        },
+      });
+
+      if (error) throw new Error(error.message);
+    } catch (error) {
       setMessage(error.message);
-    else setMessage("An unexpected error occured.");
-
-    setSubmitting(false);
+    } finally {
+      setIsLoading(false);
+      setSubmitting(false);
+    }
   };
 
   const fetchCustomerData = async (order) => {
-    setIsLoading(true);
     const payload = createPayloadFromOrder(order);
+
+    // Make the request to the server to store the card after a successful submission
     const response = await imPoweredRequest(
       "POST",
       "https://us-central1-impowered-funnel.cloudfunctions.net/funnel/checkout/quick",
       payload
     );
+
     if (!response) {
-      setMessage("Could not fetch customer data");
-    } else {
-      console.log(" => [CHARGE CARD - Fetch customer data]", response.result);
+      updateGlobalState(); // update global state with the order data
+      setMessage(
+        `We're sorry, but there was an issue processing your payment. Please try resubmitting the form or refreshing the page and trying again.`
+      );
     }
-    setIsLoading(false);
   };
 
   const createPayloadFromOrder = (order) => {
     const { product, shipping, bump } = order;
     const { line1, state, city, zip } = shipping;
-    const { name, price, product_id } = JSON.parse(product);
+    const { title, price_num, product_id } = product;
     const { cus_uuid, first_name, high_risk, funnel_uuid } = globalState;
     const payload = {
       cus_uuid,
-      product: {
-        high_risk: true,
-        title: name,
-        price,
-        product_id,
-      },
       shipping: {
         type: "BOTH",
         line1,
-        state,
+        line2: "",
         city,
+        state,
         zip,
         country: "US",
         name: first_name,
         title: "Home",
       },
+      product: {
+        high_risk: true,
+        title,
+        price: price_num,
+        product_id,
+        sku: "D8-kUSH-GUM",
+        compare_at_price: 0,
+        handle: "delta-8-strawberry-gummies",
+        option1: "4-Pack",
+        option2: "",
+        option3: "",
+        weight: 0.1,
+      },
       bump,
       high_risk,
       funnel_uuid,
     };
-    setGlobalState({ ...globalState, bump });
     return payload;
+  };
+
+  const updateGlobalState = (order = {}) => {
+    const { product = {}, bump = false } = order;
+    const { title, price_num, price_str, piece } = product;
+
+    setGlobalState({
+      ...globalState,
+      bump,
+      products: isOrderEmpty(order)
+        ? []
+        : [{ title, price_num, price_str, piece }],
+    });
+  };
+
+  const isOrderEmpty = (obj) => {
+    return Object.keys(obj).length === 0;
   };
 
   return (
