e7d15a6b2410cfc3558a538f72b62c7bda6bcea8
SammyChris <ebusameric@gmail.com>
2023-01-13 15:46:06 +0100

    feature: Add useEffect and context import for analytics and global state in Congratulations component

--- pages/congratulations.jsx
+++ pages/congratulations.jsx
@@ -1,4 +1,16 @@
+import { useContext, useEffect } from "react";
+import Footer from "../components/Footer";
+import { sendPageViewEvent } from "../lib/analytics";
+import { Context } from "../context";
+import LoadProducts from "../components/LoadProducts";
+
 const Congratulations = () => {
+  const [globalState] = useContext(Context);
+
+  useEffect(() => {
+    sendPageViewEvent("CONFIRMED"); // send page view event to google analytics
+  }, []);
+
   return (
     <div className="section-7 newt wf-section">
       <div className="container-9 w-container">
@@ -9,69 +21,31 @@ const Congratulations = () => {
           <div className="div-block-51">
             <div className="div-block-47 receipt-dividers">
               <div className="html-embed-4 icons w-embed">
-                <style
-                  dangerouslySetInnerHTML={{
-                    __html:
-                      "\n                    .check {\n                        fill: #fff;\n                        color: #fff;\n                        height: 16px;\n                        width: 16px;\n                        vertical-align: middle;\n                    }\n                ",
-                  }}
-                />
-                <div>
-                  <svg
-                    className="check"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1.25em"
-                    height="1em"
-                    viewBox="0 0 1600 1280"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M1575 310q0 40-28 68l-724 724l-136 136q-28 28-68 28t-68-28l-136-136L53 740q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295l656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
-                    />
-                  </svg>
-                </div>
+                <svg
+                  className="check"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.25em"
+                  height="1em"
+                  viewBox="0 0 1600 1280"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M1575 310q0 40-28 68l-724 724l-136 136q-28 28-68 28t-68-28l-136-136L53 740q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295l656-657q28-28 68-28t68 28l136 136q28 28 28 68z"
+                  />
+                </svg>
               </div>
               <h2 className="heading-21 receipt-headers">
                 Your Product Receipt:
               </h2>
             </div>
-            <div className="div-block-116">
-              <div className="div-block-50">
-                <p className="paragraph-29">Product</p>
-                <p className="paragraph-30">Price</p>
-              </div>
-              <div id="PURCHASED" style={{ marginTop: "1rem" }}>
-                <div id="PoductRowFirst" className="productrow">
-                  <div className="div-block-90">
-                    <div className="div-block-92">
-                      <div className="productrowtitle">
-                        (14 Giveaway Entries)
-                      </div>
-                      <div className="productrowsubheader">
-                        2 Products (1 Wristband/1 Decal)
-                      </div>
-                    </div>
-                  </div>
-                  <div className="div-block-91">
-                    <div className="productrowsubheader">$7.00 / pc</div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <LoadProducts products={globalState.products} />
             <div className="div-block-47 receipt-dividers">
               <div className="html-embed-5 icons w-embed">
-                <style
-                  dangerouslySetInnerHTML={{
-                    __html:
-                      "\n                    .lock {\n                        fill: #fff;\n                        color: #fff;\n                        height: 16px;\n                        width: 16px;\n                        vertical-align: middle;\n                    }\n                ",
-                  }}
+                <img
+                  src="/images/american-wavy-flag-clipart-free-png.webp"
+                  height={25}
+                  width={35}
                 />
-                <div>
-                  <img
-                    src="images/american-wavy-flag-clipart-free-png.webp"
-                    height={25}
-                    width={35}
-                  />
-                </div>
               </div>
               <h2 className="heading-21 receipt-headers">
                 Thank you for supporting a Veteran Owned Business!
@@ -100,26 +74,18 @@ const Congratulations = () => {
             </div>
             <div className="div-block-47 receipt-dividers">
               <div className="html-embed-5 icons w-embed">
-                <style
-                  dangerouslySetInnerHTML={{
-                    __html:
-                      "\n    .lock {\n        fill: #fff;\n        color: #fff;\n        height: 16px;\n        width: 16px;\n        vertical-align: middle;\n    }\n    ",
-                  }}
-                />
-                <div>
-                  <svg
-                    className="lock"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="0.88em"
-                    height="1em"
-                    viewBox="0 0 448 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"
-                    />
-                  </svg>
-                </div>
+                <svg
+                  className="lock"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="0.88em"
+                  height="1em"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"
+                  />
+                </svg>
               </div>
               <h2 className="heading-21 receipt-headers">Our Guarantee:</h2>
             </div>
@@ -144,46 +110,7 @@ const Congratulations = () => {
               </div>
             </div>
           </div>
-          <div className="div-block-42">
-            <div className="div-block-43 img-wrapper">
-              <img
-                src="images/bigly-logo.png"
-                loading="lazy"
-                alt="Bigly logo"
-                className="footer-img"
-              />
-            </div>
-            <div>
-              <p className="paragraph-27">
-                <a
-                  href="https://goingbigly.com/pages/privacy-policy"
-                  target="_parent"
-                >
-                  <span className="text-span-15 footer-a">Privacy Policy</span>
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://goingbigly.com/pages/terms-of-service"
-                  target="_parent"
-                >
-                  <span className="text-span-13 footer-a">
-                    Terms of Service
-                  </span>
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://goingbigly.com/pages/contact-us"
-                  target="_parent"
-                >
-                  <span className="text-span-12 footer-a">Contact Us</span>
-                  <br />
-                </a>
-                <span className="text-span-11">
-                  GoingBigly © 2021 - All Rights Reserved
-                </span>
-              </p>
-            </div>
-          </div>
+          <Footer />
         </div>
       </div>
     </div>
