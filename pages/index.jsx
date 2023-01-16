d2ecb8ea34f1833303db2a888f88ac07bc90479e
SammyChris <ebusameric@gmail.com>
2023-01-16 19:06:05 +0100

    style: change card style

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,22 +22,7 @@ export default function Home() {
 
   const options = {
     clientSecret,
-    // appearance: { theme: "stripe" },
-    style: {
-      base: {
-        fontSize: "16px",
-        fontWeight: "800",
-        color: "#424770",
-        letterSpacing: "0.025em",
-        fontFamily: "Source Code Pro, monospace",
-        "::placeholder": {
-          color: "#aab7c4",
-        },
-      },
-      invalid: {
-        color: "#9e2146",
-      },
-    },
+    appearance: { theme: "stripe" },
   };
 
   return (
