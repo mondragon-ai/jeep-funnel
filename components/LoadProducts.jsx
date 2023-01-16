8b13b3910652ec562f9f115a0093b1a3abbbc166
SammyChris <ebusameric@gmail.com>
2023-01-16 19:08:04 +0100

    style; adjust styles for product'

--- components/LoadProducts.jsx
+++ components/LoadProducts.jsx
@@ -7,16 +7,24 @@ const LoadProducts = ({ products }) => {
       </div>
       <div id="PURCHASED" style={{ marginTop: "1rem" }}>
         {products?.map((product, i) => (
-          <div id="PoductRowFirst" className="productrow" key={i}>
+          <div
+            id="PoductRowFirst"
+            className="productrow"
+            key={i}
+            style={{ height: "auto" }}
+          >
             <div className="div-block-90">
               <div className="div-block-92">
                 <div className="productrowtitle">{product.title}</div>
                 <div className="productrowsubheader">{product.piece}</div>
               </div>
             </div>
-            <div className="div-block-91">
-              <div className="productrowsubheader">
-                {product.price_str || product.price_num}
+            <div className="div-block-91" style={{ top: "auto" }}>
+              <div
+                className="productrowsubheader final-product"
+                style={{ left: "auto" }}
+              >
+                {product.price_str?.replace(/\s/g, "") || product.price_num}
               </div>
             </div>
           </div>
