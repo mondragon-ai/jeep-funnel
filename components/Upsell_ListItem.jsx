2357144484ba9e5cf7efafed6bb7284487f89eb0
SammyChris <ebusameric@gmail.com>
2023-01-17 19:11:57 +0100

    remove unwanted classnames

--- components/Upsell_ListItem.jsx
+++ components/Upsell_ListItem.jsx
@@ -2,12 +2,9 @@ import HtmlReactParser from "html-react-parser";
 
 const Upsell_ListItem = ({ text }) => {
   return (
-    <div
-      className="de elBullet elMargin0 ui-droppable mfs_18 de-editable"
-      id="tmp_list-47178-160"
-    >
-      <ul className="ne elBulletList elBulletListNew elBulletList2 listBorder0">
-        <li style={{ fontSize: "24px" }}>
+    <div className="upsell_list">
+      <ul>
+        <li>
           <i className="fa-fw fas fa-circle" />
           {HtmlReactParser(text)}
         </li>
