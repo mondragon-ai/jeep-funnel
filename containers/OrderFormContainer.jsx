06a78b6542a0aaa2d6da252b70b562a76f460941
SammyChris <ebusameric@gmail.com>
2023-01-20 19:28:53 +0100

    add square payment

--- containers/OrderFormContainer.jsx
+++ containers/OrderFormContainer.jsx
@@ -7,10 +7,20 @@ import { Context } from "../context";
 const OrderFormContainer = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const [squareCard, setSquareCard] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [status, setStatus] = useState("");
+  // const [paymentMethod, setPaymentMethod] = useState("stripe");
+  const appId = "sandbox-sq0idb-RT3u-HhCpNdbMiGg5aXuVg";
+  const locationId = "TC4Z3ZEBKRXRH";
+  let isSquareCardAttached;
+  let squareCardPayment;
+
   const [message, setMessage] = useState("");
+  const [paymentType, setPaymentType] = useState("stripe");
   const [clientOrigin, setClientOrigin] = useState("http://localhost:3000");
   const [globalState, setGlobalState] = useContext(Context);
+  const { high_risk } = globalState;
 
   const initialValues = {
     product: {
@@ -31,17 +41,97 @@ const OrderFormContainer = () => {
 
   useEffect(() => {
     setClientOrigin(window.location.origin);
+    if (!window.Square) {
+      return;
+      // throw new Error("Square.js failed to load properly");
+    }
+    try {
+      const payments = window.Square.payments(appId, locationId);
+      initializeSquareCard(payments);
+    } catch {
+      setStatus("missing-credentials");
+      return;
+    }
   }, []);
+
+  async function initializeSquareCard(payments) {
+    try {
+      if (!isSquareCardAttached) {
+        isSquareCardAttached = true;
+        squareCardPayment = await payments.card();
+        await squareCardPayment.attach("#card-container");
+
+        setSquareCard(cardPayment);
+      }
+    } catch (e) {
+      console.error("Initializing Square Card failed", e);
+      return;
+    }
+  }
+
+  // Checkpoint 2.
+  async function handlePaymentMethodSubmission(event) {
+    event.preventDefault();
+
+    try {
+      // disable the submit button as we await tokenization and make a payment request.
+      setIsLoading(true);
+      const token = await tokenize();
+      const paymentResults = await createPayment(token);
+      setStatus("is-success");
+      console.debug("Payment Success", paymentResults);
+    } catch (e) {
+      setIsLoading(false);
+      setStatus("is-failure");
+      console.error(e.message);
+    }
+  }
+
+  async function createPayment(token) {
+    const body = JSON.stringify({
+      locationId,
+      sourceId: token,
+    });
+
+    console.log("cool.ing");
+    const paymentResponse = await fetch("/payment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body,
+    });
+
+    if (paymentResponse.ok) {
+      return paymentResponse.json();
+    }
+
+    const errorBody = await paymentResponse.text();
+    throw new Error(errorBody);
+  }
+
+  async function tokenize() {
+    const tokenResult = await squareCard.tokenize();
+    if (tokenResult.status === "OK") {
+      return tokenResult.token;
+    } else {
+      let errorMessage = `Tokenization failed with status: ${tokenResult.status}`;
+      if (tokenResult.errors) {
+        errorMessage += ` and errors: ${JSON.stringify(tokenResult.errors)}`;
+      }
+
+      throw new Error(errorMessage);
+    }
+  }
 
   const handleSubmit = async (values, { setSubmitting }) => {
     try {
+      if (!stripe || !elements) return;
+
       setIsLoading(true);
 
-      if (!stripe || !elements) {
-        throw new Error("stripe or elements are not present");
-      }
-
-      updateGlobalState(values); // update global state with the order data
+      const updatedState = createNewState(values); // update global state with the order data
+      const queryString = objectToQueryString(updatedState); // get the query string from new state
 
       setTimeout(() => {
         fetchCustomerData(values); // simulate a delay
@@ -50,7 +140,7 @@ const OrderFormContainer = () => {
       const { error } = await stripe.confirmSetup({
         elements,
         confirmParams: {
-          return_url: `${clientOrigin}/upsell`,
+          return_url: `${clientOrigin}/upsell?${queryString}`,
         },
       });
 
@@ -71,9 +161,7 @@ const OrderFormContainer = () => {
       "https://us-central1-impowered-funnel.cloudfunctions.net/funnel/checkout/quick",
       payload
     );
-
     if (!response) {
-      updateGlobalState(); // update global state with the order data
       setMessage(
         `We're sorry, but there was an issue processing your payment. Please try resubmitting the form or refreshing the page and trying again.`
       );
@@ -110,6 +198,8 @@ const OrderFormContainer = () => {
         option2: "",
         option3: "",
         weight: 0.1,
+        variant_id: 42235971567788,
+        quantity: 1,
       },
       bump,
       high_risk,
@@ -118,31 +208,44 @@ const OrderFormContainer = () => {
     return payload;
   };
 
-  const updateGlobalState = (order = {}) => {
-    const { product = {}, bump = false } = order;
-    const { title, price_num, price_str, piece } = product;
+  function createNewState(values) {
+    const { title, price_num, price_str, piece } = values.product;
 
-    setGlobalState({
+    const newState = {
       ...globalState,
-      bump,
-      products: isOrderEmpty(order)
-        ? []
-        : [{ title, price_num, price_str, piece }],
-    });
-  };
+      bump: values.bump,
+      products: [{ title, price_num, price_str, piece }],
+    };
 
-  const isOrderEmpty = (obj) => {
-    return Object.keys(obj).length === 0;
+    setGlobalState(newState);
+    return newState;
+  }
+
+  const objectToQueryString = (data) => {
+    const params = new URLSearchParams();
+    Object.entries(data).forEach(([key, value]) => {
+      if (typeof value === "object") {
+        params.append(key, JSON.stringify(value));
+      } else {
+        params.append(key, value);
+      }
+    });
+    return params.toString();
   };
 
   return (
     <OrderForm
+      paymentType={paymentType}
+      setPaymentType={setPaymentType}
       initialValues={initialValues}
       handleSubmit={handleSubmit}
       isLoading={isLoading}
       message={message}
       stripe={stripe}
       elements={elements}
+      handlePaymentMethodSubmission={handlePaymentMethodSubmission}
+      status={status}
+      high_risk={high_risk}
     />
   );
 };
