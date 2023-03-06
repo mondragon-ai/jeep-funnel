3bbd3d00fe79294ff3463efc7c4aaaf42eff4605
mondragon-ai <angel.mondragon@yahoo.com>
2023-03-06 10:09:31 -0600

    ht

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { ContextProvider } from "../context";
+import { useEffect } from "react";
+import { hotjar } from 'react-hotjar';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(3395534, 6);
+  }, []);
+
   return (
     <ContextProvider>
       <Layout>
