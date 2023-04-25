038ecbefdbb8da4d89ecd1c4399d0b2e3917bb11
Angel J Mondragon <73964490+mondragon-ai@users.noreply.github.com>
2023-04-25 14:47:17 -0500

    Update next.config.js -revert

--- next.config.js
+++ next.config.js
@@ -9,9 +9,7 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-  },
-  target: 'serverless',
-  trailingSlash: true,
+  }
 };
 
 module.exports = nextConfig;
